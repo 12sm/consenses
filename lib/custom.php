05fa23943b6f7d4a88c1ad2a1273d3e08dc746c2
Ian Noyes <ian@12southmusic.com>
2014-06-12 17:47:30 -0500

    Revert "Revert "set art parent to page/post of form""
    This reverts commit 61c11ccc274b672d015e143121dec602fd402cf6.

--- lib/custom.php
+++ lib/custom.php
@@ -2,4 +2,12 @@
 
 define( 'UPLOADS', ''.'assets' );
 
+add_filter('frm_validate_field_entry', 'my_custom_validation', 8, 3);
+function my_custom_validation($errors, $posted_field, $posted_value){
+  if($posted_field->id == 106){ //change 25 to the ID of any field in your form
+    $_POST['frm_wp_post']['=post_parent'] = $_POST['item_meta'][106]; //change 30 to the ID of your WP parent page (ourmembers)
+  }
+  return $errors;
+}
+
 ?>
