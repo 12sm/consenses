1c7f4a45257c0f5731b9ada046e28f9bb481fc3e
Ian Noyes <ian@12southmusic.com>
2014-06-12 17:20:17 -0500

    Revert "im getting desperate"
    This reverts commit 9044eb7f8ec60f726ca68304d66afba6731c3e34.

--- functions.php
+++ functions.php
@@ -45,7 +45,6 @@ jQuery(document).ready(function() {
 /**
  * Roots includes
  */
-require_once locate_template('/lib/custom.php');          // Custom functions
 require_once locate_template('/lib/utils.php');           // Utility functions
 require_once locate_template('/lib/init.php');            // Initial theme setup and constants
 require_once locate_template('/lib/wrapper.php');         // Theme wrapper class
@@ -60,3 +59,4 @@ require_once locate_template('/lib/comments.php');        // Custom comments mod
 require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
+require locate_template('/lib/custom.php');          // Custom functions
