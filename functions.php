4f820059a3c9656fcd95d239cf3ed8be25fcfe88
Ian Noyes <ian@12southmusic.com>
2014-06-12 14:54:38 -0500

    put it in functions.php

--- functions.php
+++ functions.php
@@ -59,3 +59,11 @@ require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
 require_once locate_template('/lib/custom.php');          // Custom functions
+
+add_filter('frm_validate_field_entry', 'my_custom_validation', 8, 3);
+function my_custom_validation($errors, $posted_field, $posted_value){
+  if($posted_field->id == 106){ //change 25 to the ID of any field in your form
+    $_POST['frm_wp_post']['=post_parent'] = $_POST['item_meta'][106]; //change 30 to the ID of your WP parent page (ourmembers)
+  }
+  return $errors;
+}
