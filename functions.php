9044eb7f8ec60f726ca68304d66afba6731c3e34
Ian Noyes <ian@12southmusic.com>
2014-06-12 17:10:17 -0500

    im getting desperate

--- functions.php
+++ functions.php
@@ -44,6 +44,7 @@ jQuery(document).ready(function() {
 /**
  * Roots includes
  */
+require_once locate_template('/lib/custom.php');          // Custom functions
 require_once locate_template('/lib/utils.php');           // Utility functions
 require_once locate_template('/lib/init.php');            // Initial theme setup and constants
 require_once locate_template('/lib/wrapper.php');         // Theme wrapper class
@@ -58,4 +59,3 @@ require_once locate_template('/lib/comments.php');        // Custom comments mod
 require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
-require locate_template('/lib/custom.php');          // Custom functions
