f751eab9108bb6b305be2914a806b2f91d653205
Ian Noyes <ian@12southmusic.com>
2014-06-16 10:21:30 -0500

    Home template

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,4 +1,31 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php the_content(); ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
-<?php endwhile; ?>
+<div class="row home-top">
+	<div class="col-sm-6">
+		<div class="welcome-box">
+			<h2>Welcome to <span class="red">Consenses</span></h2>
+			<h4>Connecting the world <span class="red">through art</span></h4>
+		</div>
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="explore">
+					<h3>Explore</h3>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="create">
+					<h3>+ Create</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row col weekly-challenge">
+	<div class="col-sm-12">
+		<?php echo do_shortcode('[wpv-view name="HOME | WEEKLY-CHALLENGE"]'); ?>
+	</div>
+</div>
+<div class="row home-events">
+	<?php echo do_shortcode('[wpv-view name="HOME | EVENTS"]'); ?>
+</div>
+<div class="row home-news">
+	<?php echo do_shortcode('[wpv-view name="HOME | NEWS"]'); ?>
+</div>
