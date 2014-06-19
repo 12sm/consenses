8b962c18fcd1a1980a872ea14f4d16ea352672f5
Ian Noyes <ian@12southmusic.com>
2014-06-19 10:49:55 -0500

    custom scripts

--- lib/custom.php
+++ lib/custom.php
@@ -4,7 +4,11 @@ add_filter('frm_validate_field_entry', 'my_custom_validation', 8, 3);
 //Create a parent relationship after completion of form
 function my_custom_validation($errors, $posted_field, $posted_value){
   if($posted_field->id == 108){ //change 25 to the ID of any field in your form
-    $_POST['frm_wp_post']['=_wpcf_belongs_chain_id'] = $_POST['item_meta'][108]; //change 30 to the ID of your WP parent page (ourmembers)
+    $id = $_POST['frm_wp_post'];
+    $parent = $_POST['item_meta'][108];
+    update_post_meta($id,'=_wpcf_belongs_chain_id', $parent);
+    echo "$id this is the id"
+    echo "$parent this is the parent"  //change 30 to the ID of your WP parent page (ourmembers)
   }
   return $errors;
 }
