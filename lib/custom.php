06c6a8fff9927537fcae201b11ba18984a0fc8e1
Ian Noyes <ian@12southmusic.com>
2014-06-19 14:04:20 -0500

    add artist id into the mix

--- lib/custom.php
+++ lib/custom.php
@@ -1,6 +1,15 @@
 <?php 
 add_filter('frm_after_create_entry', 'create_form_parent', 41, 2);
-
+add_filter('frm_after_create_entry', 'create_artist_parent', 41, 2);
+//Grab id of 'Artist post'
+function create_artist_parent($entry_id, $form_id){
+  if($form_id == 10){
+  	global $artist_id;
+  global $frm_entry;
+  $entry = $frm_entry->getOne($entry_id);
+  $artist_id = $entry->post_id;
+	}
+}
 //Create a parent relationship after completion of form
 function create_form_parent($entry_id, $form_id){
   if($form_id == 9){ //select form id
@@ -9,6 +18,7 @@ function create_form_parent($entry_id, $form_id){
   $post_id = $entry->post_id; //gets id of child post (not working)
     $parent = $_POST['item_meta'][108]; //gets id of parent from form
     update_post_meta($post_id,'_wpcf_belongs_chain_id', $parent);
+    update_post_meta($artist_id,'_wpcf_belongs_chain_id', $parent);
     echo "$post_id is the child";
     echo "$parent this is the parent";
   }
