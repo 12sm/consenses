4e618d516b383b4fda0dbb83f0a9e4e6fb827694
Ian Noyes <ian@12southmusic.com>
2014-06-19 18:02:58 -0500

    plug the php in

--- lib/custom.php
+++ lib/custom.php
@@ -15,7 +15,7 @@ function create_artist_parent($entry_id, $form_id){
 function create_form_parent($entry_id, $form_id){
   if($form_id == 9){ //select form id
   	global $frm_entry;
-  	global $artist_id;
+  	$artist_id = $_POST['item_meta'][112];
   $entry = $frm_entry->getOne($entry_id);
   $post_id = $entry->post_id; //gets id of child post (not working)
     $parent = $_POST['item_meta'][108]; //gets id of parent from form
