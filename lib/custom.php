994e16328987ac7f9cb1ecc9ef424567504f78bd
Ian Noyes <ian@12southmusic.com>
2014-06-19 18:30:52 -0500

    gotcha

--- lib/custom.php
+++ lib/custom.php
@@ -20,7 +20,7 @@ function create_form_parent($entry_id, $form_id){
   $post_id = $entry->post_id; //gets id of child post (not working)
     $parent = $_POST['item_meta'][108]; //gets id of parent from form
     update_post_meta($post_id,'_wpcf_belongs_chain_id', $parent);
-    add_post_meta($artist_id,'_wpcf_belongs_artist_id', $parent);
+    add_post_meta($post_id,'_wpcf_belongs_artist_id', $artist_id);
     echo "$post_id is the child";
     echo "$parent this is the parent";
     echo "$artist_id";
