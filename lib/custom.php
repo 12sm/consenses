0e0a2031e1dc387a8348d156451c384ce79a416c
Ian Noyes <ian@12southmusic.com>
2014-06-19 14:14:35 -0500

    artist id too

--- lib/custom.php
+++ lib/custom.php
@@ -19,7 +19,7 @@ function create_form_parent($entry_id, $form_id){
   $post_id = $entry->post_id; //gets id of child post (not working)
     $parent = $_POST['item_meta'][108]; //gets id of parent from form
     update_post_meta($post_id,'_wpcf_belongs_chain_id', $parent);
-    update_post_meta($artist_id,'_wpcf_belongs_chain_id', $parent);
+    update_post_meta($artist_id,'_wpcf_belongs_artist_id', $parent);
     echo "$post_id is the child";
     echo "$parent this is the parent";
   }
