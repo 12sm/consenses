6f2dc5b5f28385a241593c3df16182d9e99f5b85
Ian Noyes <ian@12southmusic.com>
2014-06-19 11:15:40 -0500

    print that array

--- lib/custom.php
+++ lib/custom.php
@@ -7,7 +7,7 @@ function my_custom_validation($errors, $posted_field, $posted_value){
     $id = $_POST['frm_wp_post'];
     $parent = $_POST['item_meta'][108];
     update_post_meta($id,'_wpcf_belongs_chain_id', $parent);
-    echo "$id this is the id";
+    print_r($id);
     echo "$parent this is the parent";  //change 30 to the ID of your WP parent page (ourmembers)
   }
   return $errors;
