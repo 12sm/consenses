e6d0d245c97f669f8b4784a05c21b444487ee949
Ian Noyes <ian@12southmusic.com>
2014-06-24 10:52:32 -0500

    poor syntax

--- templates/content-page-donors.php
+++ templates/content-page-donors.php
@@ -3,8 +3,8 @@
 	<?php echo do_shortcode('[wpv-view name="DONORS | SPONSORS"]');
 		  echo do_shortcode('[wpv-view name="DONORS | 20K"]');
 		  echo do_shortcode('[wpv-view name="DONORS | 10K"]');
-		  echo do_shortcode('[wpv-view name="DONORS | 5K']);
-		  echo do_shortcode('[wpv-view name="DONORS | 1K']);
+		  echo do_shortcode('[wpv-view name="DONORS | 5K"]');
+		  echo do_shortcode('[wpv-view name="DONORS | 1K"]');
 		  ?>
 	</div>
 </div>
