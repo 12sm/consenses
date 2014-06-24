d679ecbb8c0138e5f812d4d6d498a591957193ee
Ian Noyes <ian@12southmusic.com>
2014-06-24 10:22:55 -0500

    Create donor page template

---
+++ templates/content-page-donors.php
@@ -0,0 +1,10 @@
+<div class="sponsor-page row">
+	<div class="col-sm-12">
+	<?php echo do_shortcode('[wpv-view name="DONORS | SPONSORS"]');
+		  echo do_shortcode('[wpv-view name="DONORS | 20K"]');
+		  echo do_shortcode('[wpv-view name="DONORS | 10K"]');
+		  echo do_shortcode('[wpv-view name="DONORS | 5K']);
+		  echo do_shortcode('[wpv-view name="DONORS | 1K']);
+		  ?>
+	</div>
+</div>
