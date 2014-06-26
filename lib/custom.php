82c3b8c7010d13750dad43416883bd096d17ef11
Ian Noyes <ian@12southmusic.com>
2014-06-26 15:31:10 -0500

    this

--- lib/custom.php
+++ lib/custom.php
@@ -9,6 +9,7 @@ function create_artist_parent($entry_id, $form_id){
   $entry = $frm_entry->getOne($entry_id);
   $artist_id = $entry->post_id;
   echo "<div id='artist-content'>".$artist_id."</div>";
+  update_post_meta($post_id,'_wpcf_belongs_chains_id', $parent);
 	}
 }
 //Create a parent relationship after completion of form
