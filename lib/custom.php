70a973a57523ba94e70121e205d235667d720716
Ian Noyes <ian@12southmusic.com>
2014-06-26 11:01:36 -0500

    changing slugs

--- lib/custom.php
+++ lib/custom.php
@@ -1,6 +1,6 @@
 <?php 
 add_filter('frm_after_create_entry', 'create_form_parent', 41, 2);
-add_filter('frm_after_create_entry', 'create_artist_parent', 41, 2);
+add_filter('frm_after_create_entry', 'create_artists_parent', 41, 2);
 //Grab id of 'Artist post'
 function create_artist_parent($entry_id, $form_id){
   if($form_id == 10){
@@ -19,8 +19,8 @@ function create_form_parent($entry_id, $form_id){
   $entry = $frm_entry->getOne($entry_id);
   $post_id = $entry->post_id; //gets id of child post (not working)
     $parent = $_POST['item_meta'][108]; //gets id of parent from form
-    update_post_meta($post_id,'_wpcf_belongs_chain_id', $parent);
-    add_post_meta($post_id,'_wpcf_belongs_artist_id', $artist_id);
+    update_post_meta($post_id,'_wpcf_belongs_chains_id', $parent);
+    add_post_meta($post_id,'_wpcf_belongs_artists_id', $artist_id);
     echo "$post_id is the child";
     echo "$parent this is the parent";
     echo "$artist_id";
