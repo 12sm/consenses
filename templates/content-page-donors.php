451386462b9a1e581c69aa811f30064da8f97803
Sarah <Sarah@12southmusic.com>
2014-07-02 13:52:05 -0500

    button styling

--- templates/content-page-donors.php
+++ templates/content-page-donors.php
@@ -4,7 +4,9 @@
 		<p class="donors-top-text"><strong>Thank you for contributing to Consenses!</strong>
 		<br />You’re donation will make it possible to exhibit and tour the stunning collaborative work of 130 artists from around the world.  It will allow us to get the Consenses curriculum into more schools and It will enable us to initiate future Consenses projects all of which inspire and further our mission to use art as a vehicle for connection, understanding and peace.</p>
 		<p style="text-align:center;">To make a donation, click the button below:</p>
-		<button>Make a Donation</button>
+		<a href="#">
+		<button class="btn btn-default">Make a Donation</button>
+		</a>
 	</div>
 </div>
 	<div class="col-sm-12">
