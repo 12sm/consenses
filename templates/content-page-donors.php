8e6f6eb0487c5e2c011e09e9d9692d2b2d0d8912
Sarah <Sarah@12southmusic.com>
2014-07-02 16:12:14 -0500

    donors typography

--- templates/content-page-donors.php
+++ templates/content-page-donors.php
@@ -3,7 +3,7 @@
 		<div class="col-sm-8 col-sm-offset-2">
 			<p class="donors-top-text"><strong>Thank you for contributing to Consenses!</strong>
 			<br />You’re donation will make it possible to exhibit and tour the stunning collaborative work of 130 artists from around the world.  It will allow us to get the Consenses curriculum into more schools and It will enable us to initiate future Consenses projects all of which inspire and further our mission to use art as a vehicle for connection, understanding and peace.</p>
-			<p class="make-a-donation">If you'd like to make a donation, click the button below:</p>
+			<p class="donors-top-text">If you'd like to make a donation, click the button below:</p>
 			<a href="#">
 			<button class="btn btn-default donation-btn">Make a Donation</button>
 			</a>
