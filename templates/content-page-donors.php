29100c2b1f46093dda36ce58b8ce8ba096c3dce0
Sarah <Sarah@12southmusic.com>
2014-07-02 16:01:33 -0500

    donors page

--- templates/content-page-donors.php
+++ templates/content-page-donors.php
@@ -3,12 +3,21 @@
 	<div class="col-sm-8 col-sm-offset-2">
 		<p class="donors-top-text"><strong>Thank you for contributing to Consenses!</strong>
 		<br />You’re donation will make it possible to exhibit and tour the stunning collaborative work of 130 artists from around the world.  It will allow us to get the Consenses curriculum into more schools and It will enable us to initiate future Consenses projects all of which inspire and further our mission to use art as a vehicle for connection, understanding and peace.</p>
-		<p style="text-align:center;">To make a donation, click the button below:</p>
+		<p class="make-a-donation">If you'd like to make a donation, click the button below:</p>
 		<a href="#">
 		<button class="btn btn-default">Make a Donation</button>
 		</a>
 	</div>
 </div>
+
+	<div class="col-sm-3">
+		<img src="/wp-content/themes/consenses/assets/img/atlas.png" />
+	</div>
+	<div class="col-sm-9">
+		<p>CONSENSES is a sponsored project of Fractured Atlas, a non-profit arts service organization. Contributions for the charitable purposes of CONSENSES must be made payable to Fractured Atlas only and are tax-deductible to the extent permitted by law.</p>
+	</div>
+</div>
+
 	<div class="col-sm-12">
 	<?php echo do_shortcode('[wpv-view name="DONORS | 20K"]');
 		  echo do_shortcode('[wpv-view name="DONORS | 10K"]');
