7c9e7dff5c8d456c42d31a96714eb0648447784c
Sarah <Sarah@12southmusic.com>
2014-07-02 16:15:03 -0500

    lets try this

--- templates/content-page-donors.php
+++ templates/content-page-donors.php
@@ -1,8 +1,8 @@
 <div class="sponsor-page row container">
 	<div class="row">
 		<div class="col-sm-8 col-sm-offset-2">
-			<p class="donors-top-text"><h1>Thank you for contributing to Consenses!</h1>
-			<br />You’re donation will make it possible to exhibit and tour the stunning collaborative work of 130 artists from around the world.  It will allow us to get the Consenses curriculum into more schools and It will enable us to initiate future Consenses projects all of which inspire and further our mission to use art as a vehicle for connection, understanding and peace.</p>
+			<h1>Thank you for contributing to Consenses!</h1>
+			<p class="donors-top-text">You’re donation will make it possible to exhibit and tour the stunning collaborative work of 130 artists from around the world.  It will allow us to get the Consenses curriculum into more schools and It will enable us to initiate future Consenses projects all of which inspire and further our mission to use art as a vehicle for connection, understanding and peace.</p>
 			<p class="donors-top-text">If you'd like to make a donation, click the button below:</p>
 			<a href="#">
 			<button class="btn btn-default donation-btn">Make a Donation</button>
