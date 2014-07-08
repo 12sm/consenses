9f33a9b708c3280cd0eefdf874aefad6a903d603
Ian Noyes <ian@12southmusic.com>
2014-07-08 15:56:37 -0500

    new jquery for form submission

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,7 @@ var Roots = {
 	  $('.myzebra-add-new-term').text('Submit');
 	  $('.myzebra-add-new-term').click(function() {
       setTimeout(function(){
-		  $('#cred_form_661_1').submit();
+		  $(this).parents('form').submit();
       $('#cred_form_504_5').submit();
     }, 800);
 	  });
