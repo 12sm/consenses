750afd5e8de12af92a89ee64b21c63daa428682f
michael <michael@12southmusic.com>
2014-07-08 21:52:13 -0500

    Thank You modal

--- templates/footer.php
+++ templates/footer.php
@@ -49,6 +49,23 @@
   </div>
 </footer>
 
+<div class="modal fade thank-you">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title">Thanks for your submission!</h4>
+      </div>
+      <div class="modal-body">
+        <p>Your post will be made live shortly live pending Consenses.org review.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div><
+</div>
+
 <?php wp_footer(); ?>
 
 <!-- Begin 12SM Network Analytics <!-->   
