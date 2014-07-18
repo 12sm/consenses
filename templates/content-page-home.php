799c6b6592a625271e4a653729f8e9611fcd2178
Ian Noyes <ian@12southmusic.com>
2014-07-18 12:21:36 -0500

    push mailing list feature to home template

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -28,6 +28,29 @@
 				<?php echo do_shortcode('[wpv-view name="HOME | WEEKLY CHALLENGE | CHAIN"]'); ?>
 			</div>
 </div>
+<div class="row mail-list">
+	<div class="col-sm-6 col-sm-offset-3">
+	<h4>Stay up to date with the latest Consenses Events and Projects</h4>
+	<p>Sign up Here</p>
+		<!-- Begin MailChimp Signup Form -->
+		<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+		<style type="text/css">
+		  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+		</style>
+		<div id="mc_embed_signup">
+		<form action="http://consenses.us3.list-manage.com/subscribe/post?u=929115a3f25a36b9f13919a82&amp;id=a83cef7766" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		  <label for="mce-EMAIL">Subscribe to our mailing list</label>
+		  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+		    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+		    <div style="position: absolute; left: -5000px;"><input type="text" name="b_929115a3f25a36b9f13919a82_a83cef7766" value=""></div>
+		  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+		</form>
+		</div>
+
+		<!--End mc_embed_signup-->
+	</div>
+</div>
+
 <hr />
 <div class="row home-news-main col">
 	<div class="col-sm-12">
