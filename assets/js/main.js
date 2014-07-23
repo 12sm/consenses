62d2afcc50f399cdabdf0ad22de7226244c8a65d
michael <michael@12southmusic.com>
2014-07-22 22:41:54 -0500

    Add swiper js

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,6 +141,24 @@ var Roots = {
     init: function() {
       // JavaScript to be fired on the about us page
     }
+  },
+  exhibit: {
+	  init: function() {
+		var mySwiper = new Swiper('.swiper-container',{
+			pagination: '.pagination',
+			loop:true,
+			grabCursor: true,
+			paginationClickable: true
+		})
+		$('.arrow-left').on('click', function(e){
+			e.preventDefault()
+			mySwiper.swipePrev()
+		})
+		$('.arrow-right').on('click', function(e){
+			e.preventDefault()
+			mySwiper.swipeNext()
+		})		  
+	  }
   }
 };
 
