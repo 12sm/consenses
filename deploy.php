2f7b1bf3d52a2946b558236f2e55928cdfd67b36
Michael Shoup <michael@12southmusic.com>
2015-01-09 16:40:03 -0500

    from 12southdev.com

--- deploy.php
+++ deploy.php
@@ -6,5 +6,5 @@
  // Run the script - make sure that your SSH key is set with *no* password
  //if ( $_POST['payload'] ) {
   //shell_exec("./pull.sh"); 
-   echo shell_exec("git pull origin master 2>&1 ");
+   echo shell_exec("/usr/bin/git pull origin master 2>&1");
 //}
