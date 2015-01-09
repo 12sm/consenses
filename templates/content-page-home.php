ae30b7b47469ac1440138b38e7eff91b6d6ac539
michael <michael@12southmusic.com>
2015-01-09 15:44:43 -0600

    add page content to home

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -26,6 +26,13 @@
     </div>
 	</div>
 </div>
+<div class="row welcome">
+	<div class="col-sm-12">
+		<?php while (have_posts()) : the_post(); ?>
+			<?php the_content(); ?>
+		<?php endwhile; ?>
+	</div>
+</div>
 <div class="row home-events">
 	<?php echo do_shortcode('[wpv-view name="HOME | EVENTS"]'); ?>
 </div>
